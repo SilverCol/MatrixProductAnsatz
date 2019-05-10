8995908337a63fb37f15b08091f187c2b3208f6f
Mitja Vodnik <mitjavodnik@gmail.com>
2019-05-10 17:33:23 +0200

    direct diag

--- groundState.py
+++ groundState.py
@@ -1,18 +1,34 @@
 import numpy as np
+from scipy.sparse import lil_matrix
+from scipy.sparse.linalg import eigsh
 
+# Problem size
 N = 4
 DIM = 2**N
+# Hamiltonian matrix
+H = lil_matrix((DIM, DIM))
 
+# Bit iteration
 def bits(n):
     for i in range(N):
         yield n & 1
         n = n >> 1
 
+# Hamiltonian construction
 for row in range(DIM):
     for n, bit in enumerate(bits(row)): 
         if n == 0:
-            first = bit
             b1 = bit
         else:
+            if b1 == bit:
+                H[row, row] += 1
+            else:
+                H[row, row] -= 1
+                col = row ^ (3 << (n-1))
+                H[row, col] += 2
+            b1 = bit
 
-
+w, v = eigsh(H, k=1, which='SA')
+print(w)
+print(v)
+np.save('data/groundState' + str(N) + '.npy', v)
