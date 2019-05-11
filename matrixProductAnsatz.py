bf2dbddb0298e7937701e9d5292466f9aa01d559
Mitja Vodnik <mitjavodnik@gmail.com>
2019-05-11 14:29:34 +0200

    MPA function

--- matrixProductAnsatz.py
+++ matrixProductAnsatz.py
@@ -1,12 +1,12 @@
 import numpy as np
 import numpy.linalg as la
 from numpy.random import normal
-from useful import bits
+from useful import bits, mpa
 
 # Problem parameters
 N = 12
 DIM = 2**N
-ground = True
+ground = False
 periodic = False
 
 # Initialize quantum state
@@ -17,30 +17,11 @@ elif periodic: psi = np.load('data/groundState' + str(N) + 'p.npy')
 else: psi = np.load('data/groundState' + str(N) + '.npy')
 initial = psi
 
-# Create containers
-coefficients = []
-matrices = []
-
-# Do the MPA
-M = 1
-for j in range(1, N):
-
-    # Make SVD
-    psi = psi.reshape((2*M), 2**(N-j))
-    u, d, v = la.svd(psi, full_matrices=False)
-
-    # Store results
-    coefficients.append(d)
-    matrices.append([u[::2], u[1::2]])
-
-    # Prepare for next iteration
-    psi = np.matmul(np.diag(d), v)
-    M = d.size
-# Final MPA step
-matrices.append([psi[:, 0], psi[:, 1]])
+# Cacluate the MPA
+A, lambdas = mpa(psi)
 
 # Reconstruct initial state
 psi = np.array([
-    la.multi_dot([matrices[n][b] for n, b in enumerate(bits(s, N))])
+    la.multi_dot([A[n][b] for n, b in enumerate(bits(s, N))])
     for s in range(DIM) ])
 print('Max. error: %.2e' % np.amax(psi - initial))
--- useful.py
+++ useful.py
@@ -1,7 +1,41 @@
-# Bit iteration
+import numpy as np
+import numpy.linalg as la
+from numpy.random import normal
+
 def bits(n, N):
-    """ Yields the N bit sequence of an integer. """
+    """ Yields the N bit sequence of an integer n. """
     spot = 1 << (N-1)
     for i in range(N):
         yield (n & spot) >> (N-1)
         n = n << 1
+
+def mpa(psi):
+    """ Calculates the matrix product ansatz
+    for a spin chain state psi. """
+
+    DIM = psi.size
+    N = DIM.bit_length() - 1
+
+    # Create containers
+    coefficients = []
+    matrices = []
+    
+    # Do the MPA
+    M = 1
+    for j in range(1, N):
+    
+        # Make SVD
+        psi = psi.reshape((2*M), 2**(N-j))
+        u, d, v = la.svd(psi, full_matrices=False)
+    
+        # Store results
+        coefficients.append(d)
+        matrices.append([u[::2], u[1::2]])
+    
+        # Prepare for next iteration
+        psi = np.matmul(np.diag(d), v)
+        M = d.size
+    # Final MPA step
+    matrices.append([psi[:, 0], psi[:, 1]])
+
+    return matrices, coefficients,
