a794a6de0133d71c23abc93f1eb6718aedf7c4d5
Mitja Vodnik <mitjavodnik@gmail.com>
2019-05-11 11:29:37 +0200

    MPA seems to be working

--- groundState.py
+++ groundState.py
@@ -1,24 +1,19 @@
 import numpy as np
 from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import eigsh
+from useful import bits
 
 # Problem size
-N = 12
+N = 4
 DIM = 2**N
 periodic = False 
 # Hamiltonian matrix
 H = lil_matrix((DIM, DIM))
 
-# Bit iteration
-def bits(n):
-    for i in range(N):
-        yield n & 1
-        n = n >> 1
-
 # Hamiltonian construction
 for row in range(DIM):
-    print('\r Row %d/%d' % (row, DIM), end='\r')
-    for n, bit in enumerate(bits(row)): 
+    print('Row %d/%d' % (row, DIM), end='\r')
+    for n, bit in enumerate(bits(row, N)): 
         if n == 0:
             if periodic: first = bit
             b1 = bit
@@ -37,7 +32,7 @@ for row in range(DIM):
             H[row, row] -= 1
             col = row ^ (1 << (N-2) + 1)
             H[row, col] += 2
-print(H.toarray())
+print()
 
 # Diagonalize
 w, v = eigsh(H, k=1, which='SA')
